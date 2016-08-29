c20c833e9f43a5a0233620cb29bedc7693259407
Mikhail Fursov <fursovm@amazon.com>
2016-08-29 12:34:53 -0700

    Code cleanup

--- background.js
+++ background.js
@@ -4,21 +4,13 @@ chrome.contextMenus.create({
     onclick: process
 });
 
-function openTab(title, text, url) {
-    chrome.tabs.getSelected(null, function (tab) {
-        chrome.tabs.create({
-            "index": tab.index + 1,
-            "url": "http://investflow.ru/zametki/new?title=" + title + "&text=" + text + "&url=" + url,
-            "selected": true
-        });
-    });
-}
 function process(info) {
-    chrome.tabs.getSelected(null, function (tab) { //<-- "tab" has all the information
+    chrome.tabs.query({active: true}, function (tabs) {
+        var tab = tabs[0];
         var title = tab.title;
         var text = info.selectionText ? info.selectionText : "";
         var url = info.linkUrl ? info.linkUrl : info.srcUrl ? info.srcUrl : info.pageUrl ? info.pageUrl : info.frameUrl ? info.frameUrl : tab.url;
-        openTab(title, text, url);
+        chrome.tabs.create({"url": "http://investflow.ru/zametki/new?title=" + title + "&text=" + text + "&url=" + url});
     });
 }
 
